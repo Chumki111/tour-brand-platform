fbe508e8f68795fd82f7c33023a902f5f2d3cd19
Chumki khatun <chumkikhatun370@gamil.com>
2023-12-30 00:02:17 +0600

    initial project setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+  export default Router;
