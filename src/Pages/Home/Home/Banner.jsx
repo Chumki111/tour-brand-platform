7bbf1492853494fb42c0ea26c4d6a1cf781a7906
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-04 08:53:30 +0600

    addded banner aos animation and typewriter

--- src/Pages/Home/Home/Banner.jsx
+++ src/Pages/Home/Home/Banner.jsx
@@ -15,9 +15,17 @@ import { EffectFade, Navigation, Pagination } from 'swiper/modules';
 
 import Lottie from "lottie-react";
 import shopAnimation from '../../../../public/Animation - 1704333661162.json';
+import { Typewriter } from 'react-simple-typewriter';
+import { useEffect } from 'react';
+import Aos from 'aos';
 
 
 const Banner = () => {
+  const job = [" Welcome Back To Brand Tour"];
+  useEffect(() =>{
+    Aos.init()
+  },[])
+  
   return (
     <div className='md:h-[500px] h-full'>
       <Swiper
@@ -32,12 +40,13 @@ const Banner = () => {
       >
         <SwiperSlide>
           <div className='flex flex-col md:flex-row items-center justify-around  gap-5'>
-            <div className='md:mt-16 mt-10 md:pl-10 pl-12'>
-              <h1 className='md:text-3xl text-2xl font-bold py-3'>Welcome Back To Brand Tour</h1>
-              <p className='max-w-md'>Embark on a journey through the most coveted brands and collections from around the globe, all in one virtual destination. Whether you're a fashion enthusiast, a tech-savvy individual, or someone seeking unique lifestyle products, our platform offers an exclusive tour of top-tier brands, bringing you closer to the latest trends, timeless classics, and innovative creations.</p>
+            <div className='md:mt-16 mt-10 md:pl-10 pl-12' data-aos="fade-right">
+              <h1 className='md:text-3xl text-2xl font-bold py-3'>
+              <Typewriter words={job} loop={20} cursor/></h1>
+              <p className='max-w-md'>Embark on a journey through the most coveted brands and collections from around the globe, all in one virtual destination. Whether you`re a fashion enthusiast, a tech-savvy individual, or someone seeking unique lifestyle products, our platform offers an exclusive tour of top-tier brands, bringing you closer to the latest trends, timeless classics, and innovative creations.</p>
               <button className="py-2 w-40 h-16 px-6 mb-4 mt-6 text-black shadow-lg before:block before:-left-1 before:-top-1 before:bg-black before:absolute before:h-0 before:w-0 before:hover:w-[100%] before:hover:h-[100%]  before:duration-500 before:-z-40 after:block after:-right-1 after:-bottom-1 after:bg-black after:absolute after:h-0 after:w-0 after:hover:w-[100%] after:hover:h-[100%] after:duration-500 after:-z-40 bg-gray-200 relative inline-block font-semibold">Explore</button>
             </div>
-            <div className='lg:w-1/2'>
+            <div className='lg:w-1/2' data-aos="fade-left">
               <Lottie animationData={shopAnimation} className='w-full' />
             </div>
           </div>
