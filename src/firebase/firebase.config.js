70e7ded7c726b5bccba4e97cb79d2c315f878399
Chumki khatun <chumkikhatun370@gmail.com>
2024-01-08 17:07:10 +0600

    firebase install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdV0jwy3toy-4AbcD_yJvCYvk48G9MYW0",
+  authDomain: "brand-tour.firebaseapp.com",
+  projectId: "brand-tour",
+  storageBucket: "brand-tour.appspot.com",
+  messagingSenderId: "421576363339",
+  appId: "1:421576363339:web:5a93058bc634769190ecce"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
